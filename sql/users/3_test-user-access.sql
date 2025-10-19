cbc362048d4f899be8a46577cfc234a7262d3c1d
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-10-19 18:54:12 +0200

    feat: test users access

---
+++ sql/users/3_test-user-access.sql
@@ -0,0 +1,22 @@
+SELECT current_user, session_user;
+
+SET role guest;
+SELECT current_user, session_user;
+
+INSERT INTO board_game (name, description, designer, declared_minimal_player_count, declared_maximum_player_count, declared_minimal_age) VALUES
+('Catan', 'Trade, build, and settle the island of Catan.', 'Klaus Teuber', 3, 4, 10);
+
+SELECT * FROM board_game WHERE name = 'Catan';
+
+SET role casual_gamer;
+SELECT current_user, session_user;
+
+INSERT INTO rating (user_id, board_game_id, enjoyment) VALUES
+((SELECT user_id FROM app_user WHERE username = 'casual_gamer'),
+ (SELECT board_game_id FROM board_game WHERE name = 'Catan'),
+ 8);
+
+SET role god;
+SELECT current_user, session_user;
+
+DELETE FROM rating WHERE rating_id = 12;
