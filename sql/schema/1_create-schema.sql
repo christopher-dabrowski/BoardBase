a693f04d100c23bc9748c3a939dbe0626fd75773
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-10-19 14:27:15 +0200

    feat: change search_path for every session

--- sql/schema/1_create-schema.sql
+++ sql/schema/1_create-schema.sql
@@ -1,5 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS main AUTHORIZATION postgres;
 
-SET search_path TO main, public;
+-- SET search_path TO main, public;
+
+ALTER DATABASE boardbase SET search_path TO main, public;
 
 SHOW search_path;
