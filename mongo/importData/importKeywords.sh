0b3106e998ade035516ad4c3a3ad9afaff90c35a
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-12-04 17:55:19 +0100

    feat(mongo): load password from env file only if not already defined

--- mongo/importData/importKeywords.sh
+++ mongo/importData/importKeywords.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-MONGO_PASS=$(grep MONGO_PASSWORD ../../.env | cut -d '=' -f2 | tr -d "'" | tr -d '\n' | jq -sRr @uri)
+if [ -z "$MONGO_PASS" ]; then
+    MONGO_PASS=$(grep MONGO_PASSWORD ../../.env | cut -d '=' -f2 | tr -d "'" | tr -d '\n')
+fi
+MONGO_PASS=$(echo "$MONGO_PASS" | jq -sRr @uri)
 
 jq -c '.data | to_entries | .[] | {type: .key, values: .value}' ../data/Keywords.json \
 | mongoimport --uri "mongodb://admin:${MONGO_PASS}@localhost:27017/mtg?authSource=admin" \
-  --collection keywords --type json
+  --collection keywords
