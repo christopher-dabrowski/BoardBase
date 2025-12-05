2e05f9557f6d8be487630094403992e0dc578d6e
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-12-05 13:08:38 +0100

    feat: query to find most expensive standard cards

---
+++ mongo/relations/mostExpensiveStandardCards.mongodb.js
@@ -0,0 +1,48 @@
+use('mtg');
+
+db.standard.aggregate([
+  { $unwind: '$cards' },
+  { $limit: 100 },
+  {
+    $project: {
+      _id: 0,
+      cardUuid: '$cards.uuid',
+      cardName: '$cards.name',
+      rarity: '$cards.rarity',
+      type: '$cards.type'
+    }
+  },
+  {
+    $lookup: {
+      from: 'allPrices',
+      localField: 'cardUuid',
+      foreignField: 'cardUuid',
+      as: 'priceData'
+    }
+  },
+  { $unwind: { path: '$priceData', preserveNullAndEmptyArrays: true } },
+
+  {
+    $addFields: {
+      cardMarketPrice: {
+        $arrayElemAt: [
+          { $map: { input: { $objectToArray: '$priceData.prices.paper.cardmarket.retail.normal' }, in: '$$this.v' } },
+          0
+        ]
+      },
+      cardMarketCurrency: "$priceData.prices.paper.cardmarket.currency"
+    }
+  },
+
+  { $sort: { cardMarketPrice: -1 } },
+  { $limit: 3 },
+  {
+    $project: {
+      cardName: 1,
+      rarity: 1,
+      type: 1,
+      cardMarketPrice: { $concat: [{ $toString: "$cardMarketPrice" }, " ", "$cardMarketCurrency"] },
+    }
+  }
+]);
+
